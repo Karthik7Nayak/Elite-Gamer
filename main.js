1565d508fe5bf805b02a0f04c5ab6cab4031a22d
karthiknayak <karthik.nayak@codecraft.co.in>
2019-11-27 09:19:40 +0530

    changes

--- main.js
+++ main.js
@@ -1,37 +1,59 @@
-const { app, BrowserWindow, Menu,ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 const url = require('url');
 const { autoUpdater } = require("electron-updater");
+
 
 
 Menu.setApplicationMenu(null);
 let mainWindow;
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height:900 ,
+    width: 400,
+    height: 280,
     webPreferences: {
       nodeIntegration: true
     }
   });
+  mainWindow.resizable=false;
   mainWindow.setMenuBarVisibility(false);// setAutoHideMenuBar(hide);
   // mainWindow.loadFile(`${__dirname}/index.html`);
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'dist', 'index.html'),
+    // pathname: path.join(__dirname, 'dist', 'index.html'),
+    pathname: path.join(__dirname, 'renderer', 'login.html'),
     protocol: 'file:',
     slashes: true
   }));
-  mainWindow.webContents.openDevTools();
+
+  // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 
 };
+const showHomeWindow = () => {
+  mainWindow.setSize(1200,700);
+  mainWindow.center();
+  mainWindow.resizable=true;
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist', 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+    
+  }));
+
+  // mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+  // mainWindow.
+}
 
 
 app.on('ready', () => {
   createWindow();
-  autoUpdater.checkForUpdatesAndNotify();
+  // autoUpdater.checkForUpdatesAndNotify();
 });
 
 app.on('window-all-closed', () => {
@@ -52,3 +74,10 @@ autoUpdater.on('update-downloaded', () => {
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
 });
+
+
+ipcMain.on('login-success', () => {
+  showHomeWindow();
+
+  // mainWindow.close();
+})
